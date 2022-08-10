a34dd470f18073a8e6131a7baaf4aaa5267a62ce
Abhijith <abhijithjnk2001@gmail.com>
2022-08-10 12:50:21 +0530

    Counter web app complete

--- People-Counter/index.js
+++ People-Counter/index.js
@@ -1,1 +1,21 @@
-document.getElementById("count").innerText=5;
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function reset() {
+    saveEl.textContent = "Previous Entries : ";
+}
+function resetIncrement() {
+    countEl.textContent = 0;
+    count = 0;
+}
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let previousEntry = count + " - ";
+    saveEl.textContent += previousEntry;
+    resetIncrement();
+}
