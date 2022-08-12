0d248675610d09f5e496f0374f8f7fec94dd1d11
Abhijith <abhijithjnk2001@gmail.com>
2022-08-13 01:19:06 +0530

    Version 1.1 complete

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,13 +1,37 @@
 let myLeads = [];
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el");
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("Leads") );
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();    
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+    localStorage.setItem("Leads",JSON.stringify(myLeads));    
     renderLeads();
-    // console.log(myLeads);
+})
+
+inputEl.addEventListener("keypress", function(e) {
+    if (e.key == "Enter") {
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        localStorage.setItem("Leads",JSON.stringify(myLeads));    
+        renderLeads();
+    }
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    renderLeads();
 })
 
 function renderLeads() {
@@ -21,3 +45,5 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems;
 }
+// localStorage.clear();
+console.log(leadsFromLocalStorage);
