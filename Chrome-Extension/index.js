5859c88a31d24a0435b96a60371671c38a120546
Abhijith <abhijithjnk2001@gmail.com>
2022-08-13 18:58:06 +0530

    Version 1.2 Complete

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -4,19 +4,39 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el");
-
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("Leads") );
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();    
+    render(myLeads);    
+}
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("Leads",JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+                    <li>
+                        <a href="${leads[i]}" target="_blank">  ${leads[i]}
+                        </a>
+                    </li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("Leads",JSON.stringify(myLeads));    
-    renderLeads();
+    render(myLeads);
 })
 
 inputEl.addEventListener("keypress", function(e) {
@@ -24,26 +44,15 @@ inputEl.addEventListener("keypress", function(e) {
         myLeads.push(inputEl.value);
         inputEl.value = "";
         localStorage.setItem("Leads",JSON.stringify(myLeads));    
-        renderLeads();
+        render(myLeads);
     }
 })
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 })
 
-function renderLeads() {
-    let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-                    <li>
-                        <a href="${myLeads[i]}" target="_blank">  ${myLeads[i]}
-                        </a>
-                    </li>`;
-    }
-    ulEl.innerHTML = listItems;
-}
 // localStorage.clear();
 console.log(leadsFromLocalStorage);
