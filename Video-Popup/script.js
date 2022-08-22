f63897d1b1033e16db9fa4951272b91139b55353
Abhijith <abhijithjnk2001@gmail.com>
2022-08-23 00:12:14 +0530

    Final version

--- Video-Popup/script.js
+++ Video-Popup/script.js
@@ -1,10 +1,15 @@
 const btnEl = document.querySelector(".btn");
 const trailerContainerEl = document.querySelector(".trailer-container");
 const closeIconEl = document.querySelector(".close-icon");
+const videoEl = document.getElementById("video-el");
 
 btnEl.addEventListener("click", ()=>{
     trailerContainerEl.classList.remove("inactive");
+    setTimeout(()=>{
+        videoEl = videoEl.play();
+    }, 500);
 })
 closeIconEl.addEventListener("click", ()=>{
     trailerContainerEl.classList.add("inactive");
+    videoEl = videoEl.load();
 })
