cea5acd7e8457b56eca577fa2ea11941d3537b9e
Abhijith <abhijithjnk2001@gmail.com>
2022-08-22 13:46:19 +0530

    Functional

---
+++ Video-Popup/script.js
@@ -0,0 +1,10 @@
+const btnEl = document.querySelector(".btn");
+const trailerContainerEl = document.querySelector(".trailer-container");
+const closeIconEl = document.querySelector(".close-icon");
+
+btnEl.addEventListener("click", ()=>{
+    trailerContainerEl.classList.remove("inactive");
+})
+closeIconEl.addEventListener("click", ()=>{
+    trailerContainerEl.classList.add("inactive");
+})
